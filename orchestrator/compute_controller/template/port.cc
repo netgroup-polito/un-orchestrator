b5e0e685edb9419663d15b4273031d730d0e9e52
ivanocerrato <8002onavi@gmail.com>
2016-12-13 10:08:00 +0100

    Replace spaces with tab

--- orchestrator/compute_controller/template/port.cc
+++ orchestrator/compute_controller/template/port.cc
@@ -5,31 +5,31 @@ Port::Port() {}
 
 
 void Port::setPortsRange(string portsRange){
-    this->portsRange = portsRange;
+	this->portsRange = portsRange;
 }
 
 void Port::setTechnology(string technology) {
-    this->technology = technology;
+	this->technology = technology;
 }
 
 string Port::getPortsRange() {
-    return  this->portsRange;
+	return  this->portsRange;
 }
 
 string Port::getTechnology() {
-    return this->technology;
+	return this->technology;
 }
 
 
 void Port::splitPortsRangeInInt(int& begin, int& end){
-    string token;
-    stringstream is(this->portsRange);
-    int i = 0;
-    while(getline(is, token, '-')) {
-        if (!i)
-            begin = atoi(token.c_str());
-        else
-            end = atoi(token.c_str());
-        i++;
-    }
+	string token;
+	stringstream is(this->portsRange);
+	int i = 0;
+	while(getline(is, token, '-')) {
+		if (!i)
+			begin = atoi(token.c_str());
+		else
+			end = atoi(token.c_str());
+		i++;
+	}
 }
--- orchestrator/compute_controller/template/port.h
+++ orchestrator/compute_controller/template/port.h
@@ -4,15 +4,15 @@ using namespace std;
 
 class Port{
 private:
-    string portsRange;
-    string technology;
+	string portsRange;
+	string technology;
 
 public:
-    Port();
-    void setPortsRange(string portsRange);
-    void setTechnology(string technology);
-    string getPortsRange();
-    string getTechnology();
-    void  splitPortsRangeInInt(int& begin, int& end);  //it splits portsRange in integers,so i can add in a map each port with the appropriate technology through a loop
+	Port();
+	void setPortsRange(string portsRange);
+	void setTechnology(string technology);
+	string getPortsRange();
+	string getTechnology();
+	void  splitPortsRangeInInt(int& begin, int& end);  //it splits portsRange in integers,so i can add in a map each port with the appropriate technology through a loop
 
 };
