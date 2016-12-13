e73f4108db1c891d34ab57c79cc8986154c2506c
ivanocerrato <8002onavi@gmail.com>
2016-12-13 08:13:24 +0100

    Updated pointer to the GUI + code cleanup

--- orchestrator/compute_controller/template/NFtemplate.cc
+++ orchestrator/compute_controller/template/NFtemplate.cc
@@ -10,59 +10,59 @@ NFtemplate::~NFtemplate() {
 }
 
 void NFtemplate::setCapability(string capability) {
-    this->capability = capability;
+	this->capability = capability;
 }
 
 void NFtemplate::setExpandable(bool expandable) {
-    this->expandable = expandable;
+	this->expandable = expandable;
 }
 
 void NFtemplate::setURI(string uri) {
-    this->uri = uri;
+	this->uri = uri;
 }
 
 void NFtemplate::setURIType(string uriType) {
-    this->uriType = uriType;
+	this->uriType = uriType;
 }
 
 void NFtemplate::setVnfType(string type) {
-    this->vnfType = type;
+	this->vnfType = type;
 }
 
 void NFtemplate::addPort(Port port) {
-    this->ports.push_back(port);
+	this->ports.push_back(port);
 }
 
 bool NFtemplate::getExpandable() {
-    return this->expandable;
+	return this->expandable;
 }
 
 string NFtemplate::getCapability() {
-    return this->capability;
+	return this->capability;
 }
 
 string NFtemplate::getURI() {
-    return this->uri;
+	return this->uri;
 }
 
 string NFtemplate::getVnfType() {
-    return this->vnfType;
+	return this->vnfType;
 }
 
 list<Port>& NFtemplate::getPorts() {
-    return this->ports;
+	return this->ports;
 }
 
 void NFtemplate::setCores(int cores) {
-    this->cores = cores;
+	this->cores = cores;
 }
 
 int NFtemplate::getCores() {
-    return this->cores;
+	return this->cores;
 }
 
 string NFtemplate::getURIType() {
-    return this->uriType;
+	return this->uriType;
 }
 
 
--- orchestrator/compute_controller/template/NFtemplate.h
+++ orchestrator/compute_controller/template/NFtemplate.h
@@ -8,31 +8,29 @@
 using namespace std;
 class NFtemplate {
 private:
-    string capability;
-    bool expandable;
-    string uri;
-    string uriType;
-    string vnfType;
-    list<Port> ports;
-    int cores;
+	string capability;
+	bool expandable;
+	string uri;
+	string uriType;
+	string vnfType;
+	list<Port> ports;
+	int cores;
 
 public:
-    NFtemplate();
-    ~NFtemplate();
-    void setCapability(string capability);
-    string getCapability();
-    void setExpandable(bool expandable);
-    bool getExpandable();
-    void setURI(string uri);
-    string getURI();
-    void setVnfType(string type);
-    string getVnfType();
-    void addPort(Port port);
-    list<Port>& getPorts();
-    void setCores(int cores);
-    int getCores();
-    string getURIType();
-    void setURIType(string uriType);
-
-
+	NFtemplate();
+	~NFtemplate();
+	void setCapability(string capability);
+	string getCapability();
+	void setExpandable(bool expandable);
+	bool getExpandable();
+	void setURI(string uri);
+	string getURI();
+	void setVnfType(string type);
+	string getVnfType();
+	void addPort(Port port);
+	list<Port>& getPorts();
+	void setCores(int cores);
+	int getCores();
+	string getURIType();
+	void setURIType(string uriType);
 };
