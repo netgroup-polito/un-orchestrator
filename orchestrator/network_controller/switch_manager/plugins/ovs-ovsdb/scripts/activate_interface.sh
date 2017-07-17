edb6e355e041f14297e8d0076cd8b25738d8cadb
ivanocerrato <8002onavi@gmail.com>
2017-07-17 14:18:06 +0200

    The un-orchestrator takes care of detaching interfaces from the network manager: * interfaces provided through the ini file * interfaces created dynamically and attached to VNFs

--- orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/scripts/activate_interface.sh
+++ orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/scripts/activate_interface.sh
@@ -28,4 +28,23 @@ echo "[$0] Activatig interface $1"
 sudo ethtool --offload $1 rx off tx off &> /dev/null
 sudo ethtool -K $1 gso off &> /dev/null
 
+# Remove the ip address from the itnerface(rofl
+sudo ifconfig $1 0
+
+# Disable the network manager on the interface
+
+address=`ifconfig $1 | grep HWaddr  | awk {'print $5'}`
+
+if [ `cat /etc/NetworkManager/NetworkManager.conf  | grep unmanaged-devices | wc -l` -eq 0 ]
+then
+	echo "[keyfile]" >> /etc/NetworkManager/NetworkManager.conf
+	echo "unmanaged-devices=mac:$address" >> /etc/NetworkManager/NetworkManager.conf
+else
+	string=`cat /etc/NetworkManager/NetworkManager.conf | grep unmanaged-devices`
+	new=`echo $string $address`
+	ok=`echo ${new// /;}`
+	sed "s/$string/$ok/g" /etc/NetworkManager/NetworkManager.conf > /etc/NetworkManager/un-tmp.conf
+	mv /etc/NetworkManager/un-tmp.conf /etc/NetworkManager/NetworkManager.conf
+fi
+
 exit 0
