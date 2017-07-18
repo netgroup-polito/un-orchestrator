60e670d6ab785bffc55e248c7363eec21ffb28b6
ivanocerrato <8002onavi@gmail.com>
2017-07-18 14:29:16 +0200

    code cleanup

--- orchestrator/node_resource_manager/rest_server/rest_server.cc
+++ orchestrator/node_resource_manager/rest_server/rest_server.cc
@@ -219,6 +219,13 @@ void RestServer::login(const Rest::Request& request, Http::ResponseWriter respon
 // POST /users/:name
 void RestServer::createUser(const Rest::Request& request, Http::ResponseWriter response) 
 {
+	if (dbmanager == NULL) {
+		ULOG_INFO("Login can be performed only if authentication is enabled through the configuration file");
+		//return httpResponse(connection, MHD_HTTP_NOT_IMPLEMENTED);
+		response.send(Http::Code::Not_Implemented);
+		return;
+	}
+
 	//Retrieve the username from the URL
 	auto username = request.param(":name").as<std::string>();
 	
@@ -323,6 +330,13 @@ void RestServer::createUser(const Rest::Request& request, Http::ResponseWriter r
 // GET /users/:name
 void RestServer::getUser(const Rest::Request& request, Http::ResponseWriter response) 
 {
+	if (dbmanager == NULL) {
+		ULOG_INFO("Login can be performed only if authentication is enabled through the configuration file");
+		//return httpResponse(connection, MHD_HTTP_NOT_IMPLEMENTED);
+		response.send(Http::Code::Not_Implemented);
+		return;
+	}
+
 	//Retrieve the username from the URL
 	auto username = request.param(":name").as<std::string>();
 	
@@ -513,9 +527,6 @@ int RestServer::answer_to_connection(void *cls,
 	if (NULL == *con_cls) {
 		ULOG_INFO("New %s request for %s using version %s", method, url, version);
 
-		if (LOGGING_LEVEL <= ORCH_DEBUG)
-			MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
-
 		struct connection_info_struct *con_info;
 		con_info = (struct connection_info_struct*) malloc(
 				sizeof(struct connection_info_struct));
@@ -584,12 +595,6 @@ int RestServer::answer_to_connection(void *cls,
 	}
 
 	//Just to remove a warning in the compiler
-	return MHD_YES;
-}
-
-int RestServer::print_out_key(void *cls, enum MHD_ValueKind kind,
-		const char *key, const char *value) {
-	ULOG_DBG("%s: %s", key, value);
 	return MHD_YES;
 }
 
